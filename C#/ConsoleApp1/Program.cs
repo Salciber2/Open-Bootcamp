92500e55e877fe242ad76ba2ba4b87e650a85f87
Salciber <121319176+Salciber2@users.noreply.github.com>
2023-02-19 22:52:48 -0300

    Add files via upload

---
+++ C#/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Ejercicio 1
+ * Escribe un programa que reciba tu nombre y lo escriba por consola.
+ */
+
+using Microsoft.VisualBasic;
+
+Console.Write("Escribe tu nombre: ");
+string nombre = Console.ReadLine();
+Console.WriteLine("¡Hola " + nombre + "!");
+
+/*
+ * Ejercicio 2
+ * Escribe un programa que tome la hora y la escriba por pantalla.
+ */
+
+Console.WriteLine("La hora es: " + DateAndTime.TimeString);
