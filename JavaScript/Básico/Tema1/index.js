62ebbd40dbe3a2cc5bad772748569ef1aaecebc6
Salciber <121319176+Salciber2@users.noreply.github.com>
2023-02-27 21:01:51 -0300

    Add files via upload

---
+++ JavaScript/Básico/Tema1/index.js
@@ -0,0 +1,11 @@
+// Mensaje de bienvenida
+console.log("Esta es la puerta de entrada al proyecto")
+
+// Comentario de una sola línea
+
+/*
+    Comentario multilínea
+*/
+
+// Mensaje de despedida
+console.log("Chau")
